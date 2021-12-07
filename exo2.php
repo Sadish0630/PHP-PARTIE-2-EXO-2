a3fe8e49451e40b1791b8e61181ffbc489b35196
audemar <audemarsadish@gmail.com>
2021-12-07 16:11:08 +0100

    exo2 terminé

---
+++ exo2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css"/>
+    <title>PARTIE 2 EXO 1</title>
+</head>
+<body>
+<p>
+   <?php
+    $age = 40;
+    if ($age >= 18){
+   echo 'Vous êtes majeur' ;
+    }
+   else {
+   echo 'Vous êtes mineur' ;
+   }
+   ?>
+   </p>
+
+</body>
+</html>
